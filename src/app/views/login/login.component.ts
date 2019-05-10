c6657e4f2ac9893e673d5878b0c1b82510dac006
Faisal Zulfiqar <faisalzulfiqar1337@gmail.com>
2019-05-10 12:54:11 +0500

    feat: implemented register and signin

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,7 +1,29 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../core/services/api.service';
+import { Router } from '@angular/router';
+import { SharedService } from '../../shared/services/shared.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: 'login.component.html'
 })
-export class LoginComponent { }
+export class LoginComponent {
+  email: string;
+  password: string;
+  loginErrors: Map<string, string> = new Map<string, string>();
+
+  constructor(
+    private api: ApiService, 
+    private router: Router,
+    private sharedService: SharedService) {}
+
+  submit() {
+    this.api.post('u/login', {email: this.email, password: this.password}).subscribe(
+      next => {
+        localStorage.setItem('id_token', next.token);
+        this.router.navigate(['dashboard']);
+      },
+      error => this.loginErrors = this.sharedService.errorObjToMap(error.errors)
+    )
+  }
+}
--- src/app/views/register/register.component.ts
+++ src/app/views/register/register.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../core/services/api.service';
+import { Router } from '@angular/router';
+import { SharedService } from '../../shared/services/shared.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: 'register.component.html'
 })
 export class RegisterComponent {
+  user: any = {};
+  registerErrors: Map<string, string> = new Map<string, string>();
 
-  constructor() { }
+  constructor(
+    private api: ApiService,
+    private router: Router,
+    private sharedService: SharedService) { }
 
+  submit() {
+    if (this.user.password !== this.user.confirmPassword) {
+      this.registerErrors.set('confirmPassword', 'Password does not match');
+      return;
+    }
+
+    delete this.user.confirmPassword;
+
+    this.api.post('u/signup', this.user).subscribe(
+      () => this.router.navigate(['dashboard']),
+      error => this.registerErrors = this.sharedService.errorObjToMap(error.errors)
+    );
+  }
 }
