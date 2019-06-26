2891154d86514624dc0ca0a09db3d91a1d69a6a3
MohammadHasham <muhammad.hasham.2311@gmail.com>
2019-06-26 13:29:56 +0000

    fix: added more counts for dashboard.

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -4,6 +4,9 @@ import { FamilyService } from '../../core/services/family.service';
 import { DealerService } from '../../core/services/dealer.service';
 import { ProductService } from '../../core/services/product.service';
 import { forkJoin } from 'rxjs';
+import { CountryService } from '../../core/services/country.service';
+import { UserService } from '../../core/services/user.service';
+import { OrderService } from '../../core/services/order.service';
 
 @Component({
   templateUrl: 'dashboard.component.html'
@@ -12,24 +15,33 @@ export class DashboardComponent implements OnInit {
 
   shadeCount: number = 0;
   familyCount: number = 0;
-  dealerCount: number = 0;
+  countryCount: number = 0;
   productCount: number = 0;
+  userCount: number = 0;
+  orderCount: number = 0;
+
   constructor(
     private shadeService: ShadeService,
     private familyService: FamilyService,
-    private dealerService: DealerService,
+    private countryService: CountryService,
+    private userService: UserService,
+    private orderService: OrderService,
     private productService: ProductService) {
       forkJoin(
         shadeService.getAll(),
         familyService.getAll(),
-        dealerService.getAll(),
-        productService.getAll()
+        countryService.getAll(),
+        productService.getAll(),
+        userService.getAll(),
+        orderService.getAll()
       ).subscribe(
         next=> {
         this.shadeCount = next[0].length,
         this.familyCount = next[1].length,
-        this.dealerCount = next[2].length,
-        this.productCount = next[3].length
+        this.countryCount = next[2].length,
+        this.productCount = next[3].length,
+        this.userCount = next[4].length,
+        this.orderCount = next[5].length
         }
       )
    }
